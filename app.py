63d61ba3c3dcd74315f24c595630a0743f3eb7fd
Teemmii <58445475+Teemmii@users.noreply.github.com>
2022-09-28 23:51:27 -0500

    Update app.py

--- app.py
+++ app.py
@@ -118,7 +118,7 @@ def main():
 	choice = st.sidebar.selectbox("Choose a Task", activity)
 
 	# Load file
-	df = pd.read_csv("https://github.com/Teemmii/Customer-Churn-App/blob/60abb745cd276236f9844c9399569892417aefcd/BankChurners3.csv")
+	df = pd.read_csv('https://github.com/Teemmii/Customer-Churn-App/blob/ba4d1b4cf418d48e887d7f69edcd85fc13c09316/BankChurners3.CSV', header=None)
 	#EDA
 	if choice == 'EDA':
 		st.subheader("EDA Section")
